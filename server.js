bcf6e4195682efa3f384385f67affca9e5784426
goldfly <goldfly1@gmail.com>
2020-12-14 01:36:49 -0500

    striping dross from live data very sucessful. To tired to figure out lead elements.

--- server.js
+++ server.js
@@ -24,37 +24,48 @@ app.listen(port, function () {
 //stock data aggretion
 
 var axios = require("axios").default;
-var fs = require("fs");
+
 var options = {
   method: 'GET',
   url: 'https://twelve-data1.p.rapidapi.com/time_series',
-  params: {symbol: 'ibm', interval: '1day', outputsize: '5', format: 'json'},
+  params: {symbol: 'AMZN', interval: '1day', outputsize: '30', format: 'json'},
   headers: {
     'x-rapidapi-key': '30b20b11demshf65fd60638895c6p17e8ccjsn0b391d09b80f',
     'x-rapidapi-host': 'twelve-data1.p.rapidapi.com'
   }
 };
-stockData(
-axios.request(options).then(function (response) {
-  //console.log((response.data) );
-  const parsedJSON = JSON.stringify(response.data);
 
-    {fs.writeFileSync('./StocksParsed.txt', parsedJSON, function (err) {
+function stockData(){axios.request(options).then(function (response) {
+  console.log(response.data);
+  {var strJSON = JSON.stringify(response.data);
+    strJSON = strJSON.replace(/"},{"datetime":"/g, ",")
+    strJSON = strJSON.replace(/","open":"/g, ",")
+    strJSON = strJSON.replace(/","high":"/g, ",")
+    strJSON = strJSON.replace(/","low":"/g, ",")
+    strJSON = strJSON.replace(/","close":"/g, ",")
+    strJSON = strJSON.replace(/","volume":"/g, ",")
+    strJSON = strJSON.replace(/"},{"/g, ",")
+    strJSON = strJSON.replace(/{"meta":{/g, "")
+    strJSON = strJSON.replace(/"}],"status":"ok"}/g, "")
+    console.log(( strJSON) );
+
+    fs.writeFileSync('./StocksParsed.txt', strJSON, function (err) {
         console.log(err);
     });
     console.log("printed");
     }
-
-  //console.log(( parsedJSON) );
-
-  /*for( let prop in parsedJSON ){
-    console.log( prop);
-    Object.keys(parsedJSON).forEach(item => console.log(item))
-}*/
 }).catch(function (error) {
-	console.error(error + "kale");
-}));
+	console.error(error);
+});
+
+
+
+
+
+}
+
+
 //stockData("hog","10min","10","json");
-function stockData(){
+stockData();{
 console.log("piggy");
 }
