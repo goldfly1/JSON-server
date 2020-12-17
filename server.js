1c9042b4c1d31f6a1862efedda3be23af31af5ae
goldfly <goldfly1@gmail.com>
2020-12-17 16:30:41 -0500

    A start on making calculations for buy List

--- server.js
+++ server.js
@@ -1,62 +1,79 @@
-const express = require('express');
+const express = require("express");
 const axios = require("axios").default;
-const fs = require('fs');
+const fs = require("fs");
 const app = express();
-const  port = 3000;
-let tickString;
+const port = 3000;
 
 //Middleware
-app.set('view engine', "ejs");
+app.set("view engine", "ejs");
 app.use(express.static("./public"));
 //Routes
 
 //root route
-app.get('/', function (req, res) {
-  res.send('Hello World')
-  res.render('index',{ticker:tickString});
+app.get("/", function (req, res) {
+  res.send("Hello World");
+  res.render("index", { ticker: tickString });
 });
 
 //boogers route
-app.get('/midnight', function(req,res){
-    res.render('index.ejs');
-
+app.get("/midnight", function (req, res) {
+  res.render("index.ejs");
 });
 
 app.listen(port, function () {
-    console.log("server is live on port:" + port)
+  console.log("server is live on port:" + port);
 });
 
 //stock data open and calculate
-let objArray = [];
-let getDataArray =[];
+jsonData = {};
+var d = new Date();
+let todaysDate = (d.getFullYear+" "+d.getMonth+" "+d.getDate)
+picksArray=[];
 // These 4 items will be inputs from DB/user
-let symbolArray=['AAPL', 'AMZN', 'IBM', 'MSFT', 'AI', 'TSLA', 'HOG', 'FFIV', 'HELE', 'KALA', 'ROG']
-let Xsymbol ;
-let Xinterval = "1day";
-let Xoutputsize = 30;
-
-for(x of symbolArray){
-  Xsymbol = x;
-  console.log(Xsymbol +" kale");
-  Xinterval = "1day";
-  Xoutputsize = 30;
-
-  jsonReader("./symbol.json", (err, customer) => {
-    if (err) {
-      console.log("Error reading file:", err);
-      return;
-    }
+let symbolArray = [
+  "AAPL",
+  "AMZN",
+  "IBM",
+  "MSFT",
+  "AI",
+  "TSLA",
+  "HOG",
+  "FFIV",
+  "HELE",
+  "KALA",
+  "ROG",
+];
+let Xsymbol = [];
+//These are here for future use
+//let Xinterval = "1day";
+//let Xoutputsize = 30;
+x = 0;
+for (Xsymbol of symbolArray) {
+  console.log(Xsymbol + " kale");
+  fs.readFile("./data/" + Xsymbol + ".json", function (err, data) {
+    jsonData = data;
     // Make the calculations and append it to the array
-    customer.order_count += 1;
-
-
-    // write the new file of calculations from the array
-
-    fs.appendFileSync("./Picks" + todaysDate + ".json", JSON.stringify(picksArray), err => {
-      if (err) console.log("Error writing file:", err);
-    });
+    console.log(jsonData.value.length());
+    picksArray[(x, 0)] = jsonData.value;
+    picksArray[(x, 1)] = jsonData.value;
+    picksArray[(x, 2)] = jsonData.value;
+    if (err) {
+      console.log("Error while reading file " + err);
+    }
   });
-console.log(objArray[x] +"kale was here");
+
+  // write the new file of calculations from the array
+  console.log(picksArray[x] + "kale was here");
+
+  fs.appendFileSync(
+    "./data/Picks " + todaysDate + ".json",
+    JSON.stringify(picksArray),
+    (err) => {
+      if (err) console.log("Error writing file:", err);
+    }
+  );
+
+  console.log(picksArray[x] + "kale was here");
 }
 /*  as you read file act on data   build sorted arrays of picked (buy or sell) stocks  OR display news / tickers    */
 /* end of data close file /stream   */
@@ -75,25 +92,14 @@ console.log(objArray[x] +"kale was here");
 /*      */
 /*      */
 
-
-
 //function ticker(symbol,open,last){
-  function stockTicker(){
 
-    for (x=0;;x++){
-      tickString=objArray[0,x] +"  "+objArray[0,x+1]+"  "+objArray[0,x+2]+"  "+objArray[0,x]+"  "+objArray[0,x+4]+"  "+objArray[0,x+5];
-      if (x=objArray.length - 1){
-        x=0;
-      }writeThis
-    }
+//stockTicker();
+/*it's a string crawling across the div when it gets long enough start clipping off it's nose*/
 
-  }
-  //stockTicker();
-  /*it's a string crawling across the div when it gets long enough start clipping off it's nose*/
-
- // }}
- // function picks(symbol,name,weekly){
-  /* USE S&P 500 I GUESS
+// }}
+// function picks(symbol,name,weekly){
+/* USE S&P 500 I GUESS
   Reommendations once per load. They are weekly after all
 
   read info      if it is up for the month and up for the week it is a buy
@@ -109,16 +115,16 @@ console.log(objArray[x] +"kale was here");
   obj has ticker name buy/sell/nothing   blurb
   */
 //  }
-  
- // function news(line){
-  /* news feed every 3=30? minutes
+
+// function news(line){
+/* news feed every 3=30? minutes
   read lines into object    object into array
   replace on news div every 30 seconds
   REMEMBER TOAST*/
-  
- // }
- // function client(name){
-  /* kinda static kinda not
+
+// }
+// function client(name){
+/* kinda static kinda not
   client info obj    fName lName id# address email(s) phone(s)
   
   client portfolio obj id#   stock when bought  price when bought    current price
@@ -128,16 +134,13 @@ console.log(objArray[x] +"kale was here");
   acount obj     cash balance   value of portfolio   value of pseudo portfolio
 
   the system is a client and has it's own account with it's own portfolio of ALL THORETICAL trades*/
- // }
-  
-  
-  
- /* function tickerData("hog","1day","30","json")
+// }
+
+/* function tickerData("hog","1day","30","json")
 
          tickerObjArray.push getData();
          tickerListArray = "IBM", "MSFT", "AI", "TSLA", "HOG", "NPM", "FFIV", "HELE", "KALA", "ROG",;
          Run the ticker use a string;
   }*/
 
-  //*End Function Declarations*//
-
+//*End Function Declarations*//
